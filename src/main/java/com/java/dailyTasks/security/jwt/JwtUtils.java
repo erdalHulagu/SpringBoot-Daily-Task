24ea44c6f822e121f4255fe86cb5e7e6ebc3b829
Erdal Hulagu <109300830+erdalHulagu@users.noreply.github.com>
2023-08-08 21:15:33 +0100

    08/08/23

--- src/main/java/com/java/dailyTasks/security/jwt/JwtUtils.java
+++ src/main/java/com/java/dailyTasks/security/jwt/JwtUtils.java
@@ -6,6 +6,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +25,13 @@ public class JwtUtils {
 	
 	public static  final String SECRETKEY ="cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
 	 
+	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+	
+	@Value("${furkanVakfiChat.app.SECRETKEY}")  // burada istersek yukarida kullanacagimiz jwt secret key yerine application.yml da se ettik
+	private String SecretKey;                   // @Value annotaionu verip yml da set ettigimiz secreti yazdik parantez icinde, ve Expiration ms olarakda aynisini yaptik
+	 
+	@Value("${furkanVakfiChat.app.JwtExpirationMs}")
+	private String JwtExpiration;
 	
 
     public String extractUsername(String token) {
@@ -69,17 +80,16 @@ public class JwtUtils {
 //                .signWith(getSignKey(), SignatureAlgorithm.HS512).compact();
 //    }
  // JWT token üretecek
- 	public String generateToken(UserDetails userDetails) {
+// 	
+    private Key getSignKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRETKEY);     /// bu ve ustteki createToken methodunun yaptigi isi Altta direk User detailsin hazir methodunu kullanarak yaprik
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String generateToken(UserDetails userDetails) {
  		return Jwts.builder().setSubject(userDetails.getUsername())
  				             .setIssuedAt(new Date(System.currentTimeMillis()))
  				             .setExpiration(new Date(System.currentTimeMillis()+1000*60*30)).
  				              signWith(getSignKey(),SignatureAlgorithm.HS512).
  				              compact();
- 	}
- 	
-
-    private Key getSignKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRETKEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+}
 }
